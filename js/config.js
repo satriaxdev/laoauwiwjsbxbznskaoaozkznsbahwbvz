1d2a6271fa5f43d11b9753b99f269cb585352410
satriaxdev <satriadeveloperz@gmail.com>
2025-11-08 16:07:49 +0700

    Add js/config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// js/config.js
+const CONFIG = {
+    API_KEY: "AIzaSyDnDfCbFNM3Iz7iKsu6o_oyzKl_smxRyeI",
+    MODEL: "gemini-2.0-flash",
+    TELEGRAM_BOT_TOKEN: "8363298073:AAEM8jg_ZxoYfLqSI6lg1dp3eyrKXyo2EtE",
+    TELEGRAM_CHAT_ID: "7290074313"
+};
